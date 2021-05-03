092be7461d120532b6e2726d1a02922afe30a0a1
Alexey Glushkov <soniccat@list.ru>
2021-05-03 23:46:11 +0300

    tuning search view

--- androidApp/src/main/java/com/aglushkov/wordteacher/androidApp/features/definitions/views/DefinitionsUI.kt
+++ androidApp/src/main/java/com/aglushkov/wordteacher/androidApp/features/definitions/views/DefinitionsUI.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.AppBarDefaults
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
@@ -31,8 +33,10 @@ import androidx.compose.material.contentColorFor
 import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
@@ -42,59 +46,24 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.aglushkov.wordteacher.androidApp.R
 import com.aglushkov.wordteacher.shared.features.definitions.vm.DefinitionsVM
 
 @Composable
-fun DefinitionsUI(component: DefinitionsVM) {
-    val defs = component.definitions.collectAsState()
-    val (text, setText) = remember { mutableStateOf("") }
+fun DefinitionsUI(vm: DefinitionsVM) {
+    val defs = vm.definitions.collectAsState()
+    var searchText by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
             CustomTopAppBar(
             ) {
-                TextField(
-                    value = text,
-                    onValueChange = setText,
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth()
-                        .background(
-                            color = MaterialTheme.colors.surface,
-                            shape = RoundedCornerShape(2.dp)
-                        ),
-                    textStyle = LocalTextStyle.current.copy(
-                        color = MaterialTheme.colors.onSurface
-                    ),
-                    leadingIcon = {
-                        Icon(
-                            painter = painterResource(R.drawable.ic_field_search_24),
-                            contentDescription = null,
-                            tint = LocalContentColor.current
-                        )
-                    },
-                    trailingIcon = {
-                        if (text.isNotEmpty()) {
-                            Icon(
-                                painter = painterResource(R.drawable.ic_field_close_24),
-                                contentDescription = null,
-                                modifier = Modifier.clickable {
-                                    setText("")
-                                },
-                                tint = LocalContentColor.current
-                            )
-                        }
-                    },
-                    singleLine = true,
-                    colors = TextFieldDefaults.textFieldColors(
-                        backgroundColor = Color.Transparent,
-                        focusedIndicatorColor = Color.Transparent,
-                        unfocusedIndicatorColor = Color.Transparent
-                    )
-                )
+                SearchView(searchText, { searchText = it }) {
+                    vm.onWordSubmitted(searchText)
+                }
             }
         },
         bottomBar = {
@@ -109,6 +78,59 @@ fun DefinitionsUI(component: DefinitionsVM) {
 //    }) {
 //        Text("hi " + defs.value.data().orEmpty().size)
 //    }
+}
+
+@Composable
+private fun SearchView(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    onImeAction: () -> Unit
+) {
+    TextField(
+        value = text,
+        onValueChange = onTextChanged,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colors.surface,
+                shape = RoundedCornerShape(2.dp)
+            ),
+        textStyle = LocalTextStyle.current.copy(
+            color = MaterialTheme.colors.onSurface
+        ),
+        leadingIcon = {
+            Icon(
+                painter = painterResource(R.drawable.ic_field_search_24),
+                contentDescription = null,
+                tint = LocalContentColor.current
+            )
+        },
+        trailingIcon = {
+            if (text.isNotEmpty()) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_field_close_24),
+                    contentDescription = null,
+                    modifier = Modifier.clickable {
+                        onTextChanged("")
+                    },
+                    tint = LocalContentColor.current
+                )
+            }
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onImeAction()
+            }
+        ),
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
 }
 
 @Composable
@@ -130,8 +152,7 @@ private fun CustomTopAppBar(
         modifier = modifier
     ) {
         Row(
-            Modifier.fillMaxWidth()
-                .padding(contentPadding),
+            Modifier.fillMaxWidth().padding(contentPadding),
             horizontalArrangement = Arrangement.Start,
             verticalAlignment = Alignment.CenterVertically,
             content = content
