bd40244a474261a1bdc5fbfafa37faf44782adf1
Alexey Glushkov <soniccat@list.ru>
2024-05-01 17:13:10 +0300

    fixes script permissions

--- backend/cron/wordteacher_renew_cert.sh
+++ backend/cron/wordteacher_renew_cert.sh
@@ -1,3 +1,4 @@
+#/bin/bash
 
 docker compose --env-file ~/wordteacher/prod.env run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d aglushkov.ru --keep-until-expiring && \
 docker exec apigateway sh -c 'nginx -s reload'
