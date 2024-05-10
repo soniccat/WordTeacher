d9b7149a2a02815f70c0dc4a4239b32466ab9f9e
Alexey Glushkov <soniccat@list.ru>
2024-05-10 16:55:46 +0300

    tune mongo backup/restore scripts, adds ftp

--- backend/mongo_backup/mongo_backup_wiktionary.sh
+++ backend/mongo_backup/mongo_backup_wiktionary.sh
@@ -1,6 +1,15 @@
 #/bin/bash
 
-date=$(date '+%Y-%m-%d %H:%M:%S')
+if [ -z ${WT_FTP_USER+x} ]; then
+  echo "WT_FTP_USER variable isn't set"
+  exit 1
+fi
+if [ -z ${WT_FTP_PASS+x} ]; then
+  echo "WT_FTP_PASS variable isn't set"
+  exit 2
+fi
+
+date=$(date '+%Y-%m-%d_%H-%M-%S')
 uri="mongodb://192.168.0.78:27017/?directConnection=true&replicaSet=rs0"
 out="/Users/Shared/WordTeacher/backups/mongo/$date"
 
@@ -9,3 +18,5 @@ echo $uri
 echo $out
 
 mongodump --uri="$uri" --out="$out" --db=wiktionary
+
+ncftpput -R -v -u $WT_FTP_USER -p $WT_FTP_PASS 192.168.0.1 usb1_1/mongo_backups "$out"
--- backend/mongo_backup/mongo_restore.sh
+++ backend/mongo_backup/mongo_restore.sh
@@ -1,9 +1,24 @@
 #/bin/bash
 
+if [ -z ${WT_FTP_USER+x} ]; then
+  echo "WT_FTP_USER variable isn't set"
+  exit 1
+fi
+if [ -z ${WT_FTP_PASS+x} ]; then
+  echo "WT_FTP_PASS variable isn't set"
+  exit 2
+fi
+if [ -z ${1+x} ]; then
+  echo "argument with ftp foler name is required"
+  exit 3
+fi
+
 uri="mongodb://192.168.0.78:27017/?directConnection=true&replicaSet=rs0"
 
 echo $uri
 echo $1
 
-mongorestore --uri="$uri" "$1"
+ncftpget -TRv -u $WT_FTP_USER -p $WT_FTP_PASS 192.168.0.1 ./ "usb1_1/mongo_backups/$1" 
+
+mongorestore --uri="$uri" "./$1"
 # add --drop to remove previous data before restoring
