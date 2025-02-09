eb515e99c998844ebb87a54380e45792bed1aa98
Alexey Glushkov <soniccat@list.ru>
2025-02-09 19:28:34 +0300

    [Android] 1.5

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.aglushkov.wordteacher"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         addManifestPlaceholders(
             buildMap {
