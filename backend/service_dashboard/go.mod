6137a87bd1b5a3179e20fe00ac531fe7635215f1
Alexey Glushkov <soniccat@list.ru>
2025-03-15 19:10:13 +0300

    [Back] start working on dashboard

---
+++ backend/service_dashboard/go.mod
@@ -0,0 +1,11 @@
+module service_dashboard
+
+go 1.22.2
+
+require tools v0.0.0
+
+replace tools => ../tools
+
+require models v0.0.0
+
+replace models => ../models
---
+++ backend/service_dashboard/internal/model/dashboard.go
@@ -0,0 +1,27 @@
+package model
+
+import "time"
+
+type Dashboard struct {
+	HeadlineBlock DashboardHeadlineBlock `json:"headlineBlock"`
+}
+
+type DashboardHeadlineBlock struct {
+	Categories []DashboardHeadlineCategory `json:"categories,omitempty" bson:"categories,omitempty"`
+}
+
+type DashboardHeadlineCategory struct {
+	CategoryName string              `json:"categoryName,omitempty" bson:"categoryName,omitempty"`
+	Headlines    []DashboardHeadline `json:"headlines,omitempty" bson:"headlines,omitempty"`
+}
+
+type DashboardHeadline struct {
+	Id             string    `json:"id,omitempty" bson:"_id,omitempty"`
+	SourceName     string    `json:"sourceName,omitempty" bson:"sourceName,omitempty"`
+	SourceCategory string    `json:"sourceCategory,omitempty" bson:"sourceCategory,omitempty"`
+	Title          string    `json:"title" bson:"title"`
+	Description    string    `json:"description,omitempty" bson:"description,omitempty"`
+	Link           string    `json:"link" bson:"link"`
+	Date           time.Time `json:"date,omitempty" bson:"date,omitempty"`
+	Creator        *string   `json:"creator,omitempty" bson:"creator,omitempty"`
+}
